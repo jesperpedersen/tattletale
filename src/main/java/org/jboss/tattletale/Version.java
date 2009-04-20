346dddb83f550c5002c03db3a93be21e3269fdf5
jesper.pedersen <jesper.pedersen@8a1c6991-d162-0410-a49c-fa53ae6bce69>
2009-04-20 20:13:30 +0000

    On to 1.1.0.Beta1

--- src/main/java/org/jboss/tattletale/Version.java
+++ src/main/java/org/jboss/tattletale/Version.java
@@ -34,7 +34,7 @@ public class Version {
   public static final String PRODUCT = "Tattletale"; 
 
   /** The version */
-  public static final String VERSION = "1.0.0.CR1";
+  public static final String VERSION = "1.1.0.Beta1";
 
   /** Full version */
   public static final String FULL_VERSION = VENDOR + " " + PRODUCT + " " + VERSION;
