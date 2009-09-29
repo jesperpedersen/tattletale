7125568a451b491b509e4510a7aa047ff08b0cda
jesper.pedersen <jesper.pedersen@8a1c6991-d162-0410-a49c-fa53ae6bce69>
2009-09-29 16:07:19 +0000

    checkstyle fixes

--- src/main/java/org/jboss/tattletale/reporting/SealedReport.java
+++ src/main/java/org/jboss/tattletale/reporting/SealedReport.java
@@ -21,22 +21,11 @@
  */
 package org.jboss.tattletale.reporting;
 
-import org.jboss.tattletale.Version;
 import org.jboss.tattletale.core.Archive;
-import org.jboss.tattletale.core.Location;
 
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedMap;
 import java.util.SortedSet;
-import java.util.TreeMap;
 
 /**
  * Sealed information report
