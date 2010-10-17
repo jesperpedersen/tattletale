a437c33d92764082569999dc092868abd5060003
jesper.pedersen <jesper.pedersen@8a1c6991-d162-0410-a49c-fa53ae6bce69>
2010-10-17 13:56:25 +0000

    [TTALE-160] [TTALE-161] Use new ivy:makepom

--- maven/snapshot.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-mvn deploy:deploy-file -Dfile=../dist/tattletale.jar -DpomFile=core/snapshot.xml -Durl=dav:https://snapshots.jboss.org/maven2 -DrepositoryId=snapshots.jboss.org
-mvn deploy:deploy-file -Dfile=../dist/tattletale-ant.jar -DpomFile=ant/snapshot.xml -Durl=dav:https://snapshots.jboss.org/maven2 -DrepositoryId=snapshots.jboss.org
-mvn deploy:deploy-file -Dfile=../dist/tattletale-maven.jar -DpomFile=maven/snapshot.xml -Durl=dav:https://snapshots.jboss.org/maven2 -DrepositoryId=snapshots.jboss.org
