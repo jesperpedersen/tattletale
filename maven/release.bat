68714c58ddab6cdd6d7dc518d7b33a390eec2dfd
jesper.pedersen <jesper.pedersen@8a1c6991-d162-0410-a49c-fa53ae6bce69>
2010-10-17 14:20:30 +0000

    [TTALE-160] [TTALE-161] Use new ivy:makepom

--- maven/release.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-mvn deploy:deploy-file -Dfile=../dist/tattletale.jar -DpomFile=core/release.xml -Durl=dav:https://svn.jboss.org/repos/repository.jboss.org/maven2 -DrepositoryId=jboss-releases
-mvn deploy:deploy-file -Dfile=../dist/tattletale-ant.jar -DpomFile=ant/release.xml -Durl=dav:https://svn.jboss.org/repos/repository.jboss.org/maven2 -DrepositoryId=jboss-releases
-mvn deploy:deploy-file -Dfile=../dist/tattletale-maven.jar -DpomFile=maven/release.xml -Durl=dav:https://svn.jboss.org/repos/repository.jboss.org/maven2 -DrepositoryId=jboss-releases
